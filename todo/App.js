ccea8910cafffcdd2d179bf351b5127e762497e1
suesanz <yadavsourav24071998@gmail.com>
2018-03-31 02:58:51 +0530

    INitial

---
+++ todo/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './app/Component/Main'
+//
+export default class App extends React.Component {
+    render() {
+        return (
+            <Main/>
+
+        );
+    }
+}
+
+
